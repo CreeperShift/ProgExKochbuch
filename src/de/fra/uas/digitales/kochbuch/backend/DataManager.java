e29b80db691b6f8f7a83416be8445ad5b08f4d7e
CreeperShift <henkes@stud.fra-uas.de>
2021-06-15 13:13:23 +0200

    impl Classes

---
+++ src/de/fra/uas/digitales/kochbuch/backend/DataManager.java
@@ -0,0 +1,45 @@
+package de.fra.uas.digitales.kochbuch.backend;
+
+import java.util.List;
+
+public class DataManager implements IDataManager{
+    @Override
+    public IRecipe getRecipeByName(String name) {
+        return null;
+    }
+
+    @Override
+    public IRecipe deleteRecipe(IRecipe recipe) {
+        return null;
+    }
+
+    @Override
+    public void editRecipe(IRecipe recipe) {
+
+    }
+
+    @Override
+    public IRecipe getRecipeByID(int id) {
+        return null;
+    }
+
+    @Override
+    public void addNewRecipe(IRecipe iRecipe) {
+
+    }
+
+    @Override
+    public List<IRecipe> getStartRecipes(int page) {
+        return null;
+    }
+
+    @Override
+    public List<IRecipe> getRecipeByTag(List<String> tags, int page) {
+        return null;
+    }
+
+    @Override
+    public List<IRecipe> getRecipeByIngredient(List<String> ingredients, int page) {
+        return null;
+    }
+}
