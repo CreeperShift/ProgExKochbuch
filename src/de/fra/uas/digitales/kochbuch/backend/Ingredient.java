3ff4520d1eae9a794fb62f07ff7e9e9ad93f9bc8
CreeperShift <henkes@stud.fra-uas.de>
2021-06-16 00:39:20 +0200

    Added ingredient class Fixed Recipe ingredient returning only the name and not amount or type

---
+++ src/de/fra/uas/digitales/kochbuch/backend/Ingredient.java
@@ -0,0 +1,8 @@
+package de.fra.uas.digitales.kochbuch.backend;
+
+/*
+Records are classes in Java16 which have getters + setters+constructor already and only accept data.
+Perfect for this. You can't write functions for it.
+ */
+public record Ingredient(String name, float amount, String unit) {
+}
