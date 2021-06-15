2ec541c6f3c5a6f78a837ea48bc905615b362e82
CreeperShift <henkes@stud.fra-uas.de>
2021-06-15 22:58:23 +0200

    RecipeImpl

--- src/de/fra/uas/digitales/kochbuch/backend/Recipe.java
+++ src/de/fra/uas/digitales/kochbuch/backend/Recipe.java
@@ -1,4 +1,47 @@
 package de.fra.uas.digitales.kochbuch.backend;
 
-public class Recipe {
+import javafx.scene.image.Image;
+
+import java.util.List;
+
+public class Recipe implements IRecipe{
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDesc() {
+        return null;
+    }
+
+    @Override
+    public Image getImage() {
+        return null;
+    }
+
+    @Override
+    public List<String> getIngredients() {
+        return null;
+    }
+
+    @Override
+    public List<String> getSteps() {
+        return null;
+    }
+
+    @Override
+    public int getRating() {
+        return 0;
+    }
+
+    @Override
+    public int getTime() {
+        return 0;
+    }
 }
